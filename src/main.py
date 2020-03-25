a494b0797eddc88c2bc6399fc87f0382c2e2cd80
Aruneko <aruneko99@gmail.com>
2020-03-25 15:33:35 +0900

    specify response type

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from models.health_check import HealthCheckResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_model=HealthCheckResponse)
 async def health_check():
     return {"status": "OK"}
