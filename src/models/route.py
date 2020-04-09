0fe1575b66417b595a52c7e1311f6ccceb998fab
Aruneko <aruneko99@gmail.com>
2020-04-09 15:28:08 +0900

    fix lint

---
+++ src/models/route.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class GeoJsonGeometry(BaseModel):
+    type: str
+    coordinates: List[List[float]]
+
+
+class GeoJsonProperty(BaseModel):
+    shade_rate: float
+    total_distance: float
+    total_minutes: int
+
+
+class GeoJsonFeature(BaseModel):
+    type: str
+    geometry: GeoJsonGeometry
+    properties: GeoJsonProperty
+
+
+class GeoJson(BaseModel):
+    type: str
+    features: List[GeoJsonFeature]
+
+
+class Point(BaseModel):
+    lat: float
+    lon: float
+
+
+class FindRouteRequest(BaseModel):
+    departure_time: Optional[datetime]
+    arrival_time: Optional[datetime]
+    departure_point: Point
+    destination_point: Point
+
+
+class FoundRouteResponse(BaseModel):
+    shortest: GeoJson
+    recommended: GeoJson
