7a65c1ab343b2c5b510425d67e1144bbed92b8c1
Aruneko <aruneko99@gmail.com>
2020-04-09 15:29:43 +0900

    add find_route endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from models.health_check import HealthCheckResponse
+
+from .models.health_check import HealthCheckResponse
+from .models.route import FoundRouteResponse, FindRouteRequest
 
 app = FastAPI()
 
@@ -7,3 +9,8 @@ app = FastAPI()
 @app.get("/", response_model=HealthCheckResponse)
 async def health_check():
     return {"status": "OK"}
+
+
+@app.post("/find_route", response_model=FoundRouteResponse)
+async def find_route(request: FindRouteRequest):
+    return {}
