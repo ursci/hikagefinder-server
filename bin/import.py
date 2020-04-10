f12cac1692051910ef6fee1d8f4d6e36d53f3cd3
Aruneko <aruneko99@gmail.com>
2020-04-10 15:37:27 +0900

    read shape file

--- bin/import.py
+++ bin/import.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+from pathlib import Path
+
+import shapefile
 
 
 def main():
+    # Settings for Argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("path")
     args = parser.parse_args()
 
-    shape_path = args.path
-    print(shape_path)
+    # Open shape file
+    shape_path = Path(args.path)
+    shape_file = shapefile.Reader(shp=shape_path.open('rb'))
+
+    # get shapes
+    shapes = shape_file.shapes()
+    for shape in shapes:
+        print(shape.shapeType)
 
 
 if __name__ == '__main__':
