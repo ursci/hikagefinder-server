4d989f5331d4edf348b83e3f299722819f241ef6
Aruneko <aruneko99@gmail.com>
2020-05-21 16:30:29 +0900

    add db definition

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
+from database import engine, Base, SessionLocal
 from models.health_check import HealthCheckResponse
 from models.route import FoundRouteResponse, FindRouteRequest
+from usecases.search_path import search_path
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/", response_model=HealthCheckResponse)
@@ -12,5 +24,6 @@ async def health_check():
 
 
 @app.post("/find_route", response_model=FoundRouteResponse)
-async def find_route(request: FindRouteRequest):
-    return {}
+async def find_route(request: FindRouteRequest, db: Session = Depends(get_db)):
+    result = search_path(db, request.departure_point, request.destination_point)
+    return result
