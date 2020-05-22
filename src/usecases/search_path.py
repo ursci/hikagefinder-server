f1911adb4ee24a8d0b7d49a28fd0fa962e07d4e9
Aruneko <aruneko99@gmail.com>
2020-05-22 14:49:10 +0900

    add recommended route finding logic
    Former-commit-id: 17ef7f67d96487344118818a6efed4b2d394d8ba
    Former-commit-id: 65def50ce9809cba8c8e33faec79c8d5970d1b4e

--- src/usecases/search_path.py
+++ src/usecases/search_path.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 from sqlalchemy.sql import text
 from sqlalchemy.orm import Session
@@ -7,14 +8,20 @@ from models.route import Point
 from models.route import FoundRouteResponse
 
 
-RAW_QUERY = """
+RAW_SHORTEST_QUERY = """
 SELECT SUM(distance), ST_AsGeoJSON(ST_LineMerge(ST_Union(geom))) 
-FROM shade_fromAtoB(:x1, :y1, :x2, :y2);
+FROM shortest_fromAtoB(:x1, :y1, :x2, :y2);
+""".strip()
+
+RAW_RECOMMENDED_QUERY = """
+SELECT SUM(distance), ST_AsGeoJSON(ST_LineMerge(ST_Union(geom)))
+FROM shade_fromAtoB(:x1, :y1, :x2, :y2, :depart_at);
 """.strip()
 
 
-def search_path(db: Session, source: Point, destination: Point):
-    sql_statement = text(RAW_QUERY)
+def search_path(db: Session, source: Point, destination: Point, departure_time: datetime):
+    # Find shortest path
+    sql_statement = text(RAW_SHORTEST_QUERY)
     args = {
         "x1": source.lon,
         "y1": source.lat,
@@ -22,7 +29,13 @@ def search_path(db: Session, source: Point, destination: Point):
         "y2": destination.lat,
     }
     nearest_destination_path = db.execute(sql_statement, args).first()
-    print(nearest_destination_path)
+    # when the result is empty, return empty geojson
+    if nearest_destination_path[0] is None:
+        dummy = {
+            "type": "FeatureCollection",
+            "features": [],
+        }
+        return {"shortest": dummy, "recommended": dummy}
     shortest_distance = nearest_destination_path[0]
     shortest_geojson = json.loads(nearest_destination_path[1])
     shortest = {
@@ -42,5 +55,41 @@ def search_path(db: Session, source: Point, destination: Point):
             }
         ],
     }
-    response = {"shortest": shortest, "recommended": shortest}
+    # Find recommended path
+    sql_statement = text(RAW_RECOMMENDED_QUERY)
+    args = {
+        "x1": source.lon,
+        "y1": source.lat,
+        "x2": destination.lon,
+        "y2": destination.lat,
+        "depart_at": departure_time.time().strftime('%H:%M:%S'),
+    }
+    recommended_destination_path = db.execute(sql_statement, args).first()
+    # when the result is empty, return empty geojson
+    if recommended_destination_path[0] is None:
+        dummy = {
+            "type": "FeatureCollection",
+            "features": [],
+        }
+        return {"shortest": dummy, "recommended": dummy}
+    recommended_distance = recommended_destination_path[0]
+    recommended_geojson = json.loads(recommended_destination_path[1])
+    recommended = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": recommended_geojson["type"],
+                    "coordinates": recommended_geojson["coordinates"],
+                },
+                "properties": {
+                    "shade_rate": 0.0,
+                    "total_distance": recommended_distance,
+                    "total_minutes": recommended_distance,
+                },
+            }
+        ],
+    }
+    response = {"shortest": shortest, "recommended": recommended}
     return response
